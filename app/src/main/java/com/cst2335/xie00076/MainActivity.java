bb22e61736f4225ee9d57b49eae83ad0e41a3769
Bing Xie <xiebing.bingxie@gmail.com>
2023-02-06 16:13:09 -0500

    Lab 4

--- app/src/main/java/com/cst2335/xie00076/MainActivity.java
+++ app/src/main/java/com/cst2335/xie00076/MainActivity.java
@@ -17,12 +17,15 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-
+    //private MainActivityBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+     //variableBinding = MainActivityBinding.inflate(getLayoutInflater());
         setContentView(R.layout.lab3activity_main);
 
         Button btn = (Button) findViewById(R.id.button2);
@@ -32,9 +35,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String str = send_email.getText().toString();
+                //an intent object specifies the page where you are and then what page you want to go
                 Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                //the information to be attached to the intent object to send to the next page
                 i.putExtra("email", str);
-                MainActivity.this.startActivity(i);
+                //MainActivity.this.
+                startActivity(i); //go to the profile page
             }
         });
     }
--- app/src/main/java/com/cst2335/xie00076/ProfileActivity.java
+++ app/src/main/java/com/cst2335/xie00076/ProfileActivity.java
@@ -22,49 +22,56 @@ public class ProfileActivity extends AppCompatActivity {
 
     //declare a tag constant
     private static final String TAG="ProfileActivity";
-    ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult()
-            , new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                        Bitmap imgBitmap = (Bitmap) data.getExtras().get("data");
-                        ImageView imgView =findViewById(R.id.imageView);
-                        imgView.setImageBitmap(imgBitmap);
-                    } else if (result.getResultCode() == Activity.RESULT_CANCELED)
-                        Log.i(TAG, "User refused to capture a picture.");
-                }
-            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-        Button btn=findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent takePictureIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(takePictureIntent.resolveActivity(getPackageManager())!=null){
-                    myPictureTakerLauncher.launch(takePictureIntent);
-                }
-            }
-        });
-
+        ImageView img=  (ImageView)findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.profile);
         Log.e(TAG,"In function: " + "onCreate");
+//       Button btn=findViewById(R.id.button2);
+//
+//        btn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent takePictureIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+//                    //myPictureTakerLauncher.launch(takePictureIntent);
+//                }
+//           }
+//        });
+
         TextView receiver_msg;
         /*get the value of email from the main activity page*/
-        receiver_msg=findViewById(R.id.editText3);
+        receiver_msg=findViewById(R.id.editText4);
         //create the get intent object
         Intent intent=getIntent();
         //receive the value by getStringExtra() method and
-        //the key must be same which is send by the main activity
+        //the key must be same name which is send by the main activity
+        //depends on the type from the other page's intent putExtra() normally, the type could be int, long, String
+        //use gerIntExtra(), getStringExtra(), getBooleanExtra() etc
         String str=intent.getStringExtra("email");
         //display the string into textview
         receiver_msg.setText(str);
     }
+
+//    ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
+//            new ActivityResultContracts.StartActivityForResult()
+//            , new ActivityResultCallback<ActivityResult>() {
+//                @Override
+//                public void onActivityResult(ActivityResult result) {
+//                    Log.e(TAG, "In function:" + " onActivityResult");
+//                    if (result.getResultCode() == Activity.RESULT_OK) {
+//                        Intent data = result.getData();
+//                        Bitmap imgBitmap = (Bitmap) data.getExtras().get("data");
+//                        ImageView imgView =findViewById(R.id.imageView);
+//                        imgView.setImageBitmap(imgBitmap);
+//                    } else if (result.getResultCode() == Activity.RESULT_CANCELED)
+//                        Log.i(TAG, "User refused to capture a picture.");
+//                }
+//            });
+
     protected void onStart() {
         super.onStart();
         Log.e(TAG, "In function:" + " onStart");
@@ -73,7 +80,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.e(TAG, "In function:" + " onResume");
-    }
+   }
 
     protected void onPause() {
         super.onPause();
@@ -90,7 +97,4 @@ public class ProfileActivity extends AppCompatActivity {
         Log.e(TAG, "In function:" + " onDestroy");
     }
 
-    protected void onActivityResult() {
-        Log.e(TAG, "In function:" + " onActivityResult");
-    }
 }
